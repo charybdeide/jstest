d2ef9917dcc7ff05a9c9f6022cc903695a98be63
Alexandra <heirloomalexis@gmail.com>
2017-10-05 18:06:30 +0300

    change

--- js.js
+++ js.js
@@ -1,3 +1,14 @@
 $(function() {
-    alert();
-}); 
+    loadsmth(function() {
+
+    });
+});
+
+function loadsmth(callback) {
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+
+    if(callback) callback();
+    else alert();
+}
+
